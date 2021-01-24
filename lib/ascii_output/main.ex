229f8678d3809e92f80d52f76bd417db3fccb69a
Eric Ridgeway <ericridgeway@gmail.com>
2021-01-24 18:19:29 -0500

    Combine 2 params that don't need to be used till innermost function, so I don't keep having to pass them seperatley till I need them
    Also clean up typespec for default "."

--- lib/ascii_output/main.ex
+++ lib/ascii_output/main.ex
@@ -3,36 +3,37 @@ defmodule AsciiOutput.Main do
   @type x :: integer
   @type y :: integer
   @type value :: any
+  @type default :: any
 
   @type input_map :: %{{x,y} => value}
   @type ascii_char_fn :: (value -> String.t)
+  @type filler :: String.t | default
 
   @type row_list :: list(col_list)
   @type col_list :: list(String.t)
 
-  @default_filler "."
 
-  @spec ascii_output(input_map, ascii_char_fn) :: row_list
-  def ascii_output(input_map, ascii_char_fn, filler \\ @default_filler) do
-    # draw_tools = {ascii_char_fn, filler}
-    Enum.reduce(1..max_y(input_map), [], &output_add_row(&1, &2, input_map, ascii_char_fn))
+  @spec ascii_output(input_map, ascii_char_fn, filler) :: row_list
+  def ascii_output(input_map, ascii_char_fn, filler \\ ".") do
+    draw_tools = {ascii_char_fn, filler}
+    Enum.reduce(1..max_y(input_map), [], &output_add_row(&1, &2, input_map, draw_tools))
     |> Enum.reverse()
   end
 
 
-  defp output_add_row(row_num, row_list, input_map, ascii_char_fn) do
+  defp output_add_row(row_num, row_list, input_map, draw_tools) do
     row =
-      Enum.reduce(1..max_x(input_map), [], &output_add_col(&1, &2, row_num, input_map, ascii_char_fn))
+      Enum.reduce(1..max_x(input_map), [], &output_add_col(&1, &2, row_num, input_map, draw_tools))
       |> Enum.reverse()
 
     [row | row_list]
   end
 
-  defp output_add_col(col_num, col_list, row_num, input_map, ascii_char_fn) do
+  defp output_add_col(col_num, col_list, row_num, input_map, {ascii_char_fn, filler}) do
     value = Map.get(input_map, {col_num, row_num})
     ascii =
       if value == nil do
-        @default_filler
+        filler
       else
         ascii_char_fn.(value)
       end
