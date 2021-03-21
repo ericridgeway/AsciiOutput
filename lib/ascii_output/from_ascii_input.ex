7a64c3d2580d7434cb32cf156e04fbdeb7ca2849
Eric Ridgeway <ericridgeway@gmail.com>
2021-03-21 17:06:11 -0400

    reorder params

--- lib/ascii_output/from_ascii_input.ex
+++ lib/ascii_output/from_ascii_input.ex
@@ -8,13 +8,13 @@ defmodule AsciiOutput.FromAsciiInput do
   @spec generate_map(ascii_input, translater) :: Main.input_map
   def generate_map(ascii_input, translater) do
     func = &row_from_ascii(&1, &2, translater)
-    do_index_thing(ascii_input, Map.new, func)
+    do_index_thing(func, ascii_input, Map.new)
   end
 
 
   defp row_from_ascii({row, row_num}, new_map, translater) do
     func = &col_from_ascii(&1, &2, row_num, translater)
-    do_index_thing(row, new_map, func)
+    do_index_thing(func, row, new_map)
   end
 
   defp col_from_ascii({".", _}, new_map, _, _), do: new_map
@@ -26,7 +26,7 @@ defmodule AsciiOutput.FromAsciiInput do
   defp lookup_translation(translater, value), do: Map.get(translater, value, value)
   defp add_val_to_map(val, map, key), do: Map.put(map, key, val)
 
-  defp do_index_thing(ascii_input, new_map, func) do
+  defp do_index_thing(func, ascii_input, new_map) do
     ascii_input
     |> Enum.with_index(1)
     |> Enum.reduce(new_map, func)
